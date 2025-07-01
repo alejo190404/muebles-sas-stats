fed0ff1046b84d53334ab7ea2df4aa42cc0edc4a
alejo190404 <alejo190404@gmail.com>
2025-07-01 02:20:20 -0500

    Feature integrated. Still must create RabbitMQ bind

--- domain/usecase/src/main/java/co/com/bancolombia/usecase/savestat/SaveStatUseCase.java
+++ domain/usecase/src/main/java/co/com/bancolombia/usecase/savestat/SaveStatUseCase.java
@@ -3,6 +3,7 @@ package co.com.bancolombia.usecase.savestat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import co.com.bancolombia.model.events.gateways.EventsGateway;
 import co.com.bancolombia.model.stat.Stat;
 import co.com.bancolombia.model.stat.gateways.StatRepository;
 import co.com.bancolombia.usecase.savestat.utils.HashUtils;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 public class SaveStatUseCase {
 
     private final StatRepository statRepository;
+    private final EventsGateway eventGateway;
 
     public Mono<Stat> saveStat(Stat stat) {
         return HashUtils.isStatHashValid(stat)
@@ -21,7 +23,8 @@ public class SaveStatUseCase {
                         LocalDateTime now = LocalDateTime.now();
                         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
                         stat.setTimestamp(now.format(formatter));
-                        return statRepository.saveStat(stat);
+                        return statRepository.saveStat(stat)
+                                .flatMap(savedStat -> eventGateway.emit(savedStat).thenReturn(savedStat));
                     } else {
                         return Mono.error(new RuntimeException("Received hash is not valid"));
                     }
