d6ebb7f878028f74cf4f6624af7ecf2a85f63bc8
alejo190404 <alejo190404@gmail.com>
2025-07-01 00:30:35 -0500

    Endpoint exposed

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,29 +1,17 @@
 package co.com.bancolombia.api;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
-@Component
+@RestController
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
+    public Mono<ServerResponse> processStat(ServerRequest serverRequest) {
+        return ServerResponse.ok().bodyValue("Works");
     }
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -2,19 +2,15 @@ package co.com.bancolombia.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import org.springframework.web.reactive.function.server.RouterFunction;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
 public class RouterRest {
     @Bean
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(GET("/api/usecase/path"), handler::listenGETUseCase)
-                .andRoute(POST("/api/usecase/otherpath"), handler::listenPOSTUseCase)
-                .and(route(GET("/api/otherusercase/path"), handler::listenGETOtherUseCase));
+        return route(POST("/stats"), handler::processStat);
     }
 }
